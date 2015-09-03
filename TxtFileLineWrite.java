78bd564ce5cbb9b79d8301eae7f3926593c537ad
Yuan Yifan <yuanyifan1993@foxmail.com>
2015-09-03 19:48:19 +0800

    Update TxtFileLineWrite.java

--- TxtFileLineWrite.java
+++ TxtFileLineWrite.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package WordsDivision;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
